78517b1b108465ec54b2fd0400884a691b0c6153
artrayme <artrayme@protonmail.com>
2021-02-04 22:15:06 +0300

    start

---
+++ src/main/java/gui/MainWindow.java
@@ -0,0 +1,19 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class MainWindow extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Scene scene = new Scene(new AnchorPane(), 100, 100);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
