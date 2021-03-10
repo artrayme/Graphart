d28a8de0f1deae304621a2d1c0d900e32e07518a
artrayme <artrayme@protonmail.com>
2021-03-11 02:25:54 +0300

    GraphInformationPane layout added

--- src/main/java/gui/toolspane/GraphInformationPane.java
+++ src/main/java/gui/toolspane/GraphInformationPane.java
@@ -1,6 +1,16 @@
 package gui.toolspane;
 
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
-public class GraphInformationPane extends Pane {
+public class GraphInformationPane extends GridPane {
+
+    public GraphInformationPane(){
+        this.add(new Label("Nodes count = "), 0, 0);
+        this.add(new Label("Arcs count = "), 0, 1);
+        this.add(new Label("Edges count = "), 0, 2);
+        this.add(new Label("Is tree = "), 0, 3);
+
+    }
 }
